15f4519afa24b96c63f7c33d9c3e419c64b8d3a1
Neema Velasco <neemavelasco@gmail.com>
2022-04-07 14:29:20 -0500

    added env example file, modified models index.js

--- models/index.js
+++ models/index.js
@@ -6,7 +6,7 @@ const ProductTag = require('./ProductTag');
 
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'SET NULL'
+  onDelete: 'CASCADE'
 });
 
 Product.belongsTo(Category, {
