22fff56c8c1ca5684b4b031ceefd7f8ce6a51bc2
Neema Velasco <neemavelasco@gmail.com>
2022-04-07 09:08:26 -0500

    added put and delete routes for tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -51,12 +51,44 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const newTag = await Tag.update(req.body, {
+      where: { 
+        id: req.params.id 
+      }
+    })
+
+    if (!newTag[0]) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+
+    res.status(200).json(newTag)
+  } catch(err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deletedTag = await Tag.destroy({ 
+      where: { 
+        id: req.params.id 
+      }
+    })
+
+    if (!deletedTag) {
+      res.status(404).json({ message: 'No tag with this id!' });
+      return;
+    }
+
+    res.json(deletedTag)
+  } catch(err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
